eda7c2d5d35481c40fc839657ec27de422cb428f
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2022-11-07 01:36:43 +0500

    first commit

---
+++ ass.js
@@ -0,0 +1,114 @@
+//ques2
+// var z="zarnain"
+// console.log("hello  " +z+ " would like to study python")
+
+//ques3
+// var a = "zarnain"
+// var z= a.toUpperCase()
+// console.log(z)
+
+//ques4
+// var s = "Albert Einstein once said,"
+// console.log(s + " 'A person who never made a mistake never tried anything new.'")
+
+
+//ques5
+// var famouse_person= "Albert Einstein once said,"
+// var msg = "'A person who never made a mistake never tried anything new.'"
+// console.log(famouse_person+msg)
+
+//ques6
+// var e ="zarnain"
+// console.log(e)
+
+
+
+
+
+
+
+
+
+
+
+//-----------------------------------------------------------practice---------------------------
+// function fruit(){
+//     var name = "banana"
+//     var price =343
+//  console.log(name)
+//  console.log(price)
+// }
+// fruit()
+
+// for( var i=0; i<5; i++){
+//     setTimeout(()=>console.log(i) ,i)
+// }
+
+// for(let  
+// i =0; i<10; i++){
+//     setTimeout(()=> console.log(i),1)
+// }
+
+// console.log(+true)
+// console.log(typeof +true)
+
+// console.log(!!"zarmaom")
+// console.log(typeof("zarmaom"))
+
+// let data ="size";
+// const bird ={
+//     size:"small"
+
+// }
+// console.log(bird[data])
+// console.log(bird["size"])
+// console.log(bird.size)
+// console.log(bird.data)
+
+// let c ={name:"zara"}
+// let d=c;
+// c.name="zar"
+// console.log(d.name);
+
+// let a =9;
+// let b = new Number(9)
+// console.log(a==b)
+// console.log(a===b)
+
+// function fruit ()
+// {
+//     console.log("apple")
+
+// }
+// fruit.name = "woof"
+// fruit()
+
+// function sum(a,b){
+//     return a+b
+// }
+// console.log(1,2)
+
+// let n=0;
+// console.log(n++)
+// console.log(++n)
+// console.log(n++)
+// console.log(n)
+
+// function getAge(...args){
+//     console.log(args)
+// }
+// getAge(44)
+
+// function getAge(){
+// 'use strict'
+// age=90;
+// console.log(age)
+// }
+// getAge()
+
+// const sum = evalv  ("34+34*6")
+// console.log(sum)
+
+const obj = {1:"a" , 2:"b" , 3:"c"}
+console.log(obj.hasOwnProperty('1'))
+console.log(obj.hasOwnProperty(1))
---
+++ test.ts
@@ -0,0 +1,6 @@
+function add(n2,n4){
+    return n2 + n4
+}
+let result = 
+add(90,22)
+console.log(result)
