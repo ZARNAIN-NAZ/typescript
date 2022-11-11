bbf561e2ad8353827ad3e21087d7ff360a26374a
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2022-11-12 01:45:13 +0500

    book ch2

---
+++ book.js
@@ -0,0 +1,43 @@
+// let status = "new"
+// let scared = true
+// if(status==="new"){
+//     console.log("welcome ")
+
+// if(scared){
+// console.log("dont worry ")
+// }
+// else{
+// console.log("u r barve!")
+// }}
+// else{
+// console.log("welsome back!")
+// }
+
+// let lang= "javascript"
+// let msg = `lets learn ${lang}`
+// console.log(msg)
+
+// let str = "new \nline "
+// let str3 = "i \nam containing  a backslash "
+// console.log(str)
+// console.log(str3)
+
+// let str = "js is fun"
+// let str1 = "js is fun"
+// console.log("same strings" , str ==str1)
+
+// let sym = Symbol("js is fun")
+// let sym3 =  Symbol("js is fun")
+// console.log("same symbols" , sym === sym3)
+
+// let terrible =  undefined
+// let lastname;
+// console.log("same undefined:" , lastname=== terrible)
+// let betteroption = null;
+// console.log("same nullL" ,lastname === betteroption)
+
+// testVariable  = 1;
+// variableTypeTest1  = typeof testVariable;
+// variableTypeTest2  = typeof (testVariable)
+// console.log(variableTypeTest1)
+// console.log(variableTypeTest2)
